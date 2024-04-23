8b5d1a7f45f41a0525b9758e70769fa33ead39a3
Roin Putkaradze <Rputkaradze@crocobet.com>
2024-04-23 15:27:30 +0400

    game for 3 players

--- app.js
+++ app.js
@@ -9,61 +9,92 @@ const io = socketIo(server);
 const path = require('path');
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 const storeData = {
-    player1: "",
-    player2: ""
+  player1: { choice: "", ready: false },
+  player2: { choice: "", ready: false },
+  player3: { choice: "", ready: false }
 };
+
+
 
 app.use(express.json());
 app.use(cors());
 
-
 io.on('connection', (socket) => {
-    console.log('A user connected');
+  console.log('A user connected');
 
-    socket.on('disconnect', () => {
-        console.log('User disconnected');
-    });
 
-    socket.on('choice', (data) => {
-        console.log('Received choice:', data);
-        const choice = data.choice;
 
-        if (!(choice === "rock" || choice === "paper" || choice === "scissors")) {
-            return socket.emit('errorMessage', { message: 'Incorrect choice' });
-        }
+  socket.on('choice', (data) => {
+    
+    console.log('Received choice:', data);
+    const choice = data.choice;
+    
+    if (!(choice === "rock" || choice === "paper" || choice === "scissors")) {
+      return socket.emit('errorMessage', { message: 'Incorrect choice' });
+    }
+  
+    if (!storeData.player1.ready) {
+      storeData.player1.choice = choice;
+      storeData.player1.ready = true;
+      socket.emit('waitingForPlayer', { message: 'Waiting for player 2 and player 3 to make a choice' });
+    } else if (!storeData.player2.ready) {
+      storeData.player2.choice = choice;
+      storeData.player2.ready = true;
+      socket.emit('waitingForPlayer', { message: 'Waiting for player 3 to make a choice' });
+    } else {
+      storeData.player3.choice = choice;
+      storeData.player3.ready = true;
+      const result = determineWinner(storeData.player1.choice, storeData.player2.choice, storeData.player3.choice);
+      io.emit('gameResult', { player1: { choice: storeData.player1.choice }, player2: { choice: storeData.player2.choice }, player3: { choice: storeData.player3.choice }, winner: result });
+      // Reset choices and readiness for the next round
 
-        if (storeData.player1) {
-            storeData.player2 = choice;
-            const result = determineWinner(storeData.player1, storeData.player2);
-            io.emit('gameResult', { player1: storeData.player1, player2: storeData.player2, winner: result });
-            storeData.player1 = "";
-            storeData.player2 = "";
-
-        } else {
-            storeData.player1 = choice;
-            socket.emit('waitingForPlayer', { message: 'Waiting for player 2 to make a choice' });
-        }
-    });
+      resetGameData();
+    }
+  });
 });
 
-function determineWinner(player1Choice, player2Choice) {
 
-    if (player1Choice === player2Choice) {
-        return 'draw';
-    } else if ((player1Choice === 'rock' && player2Choice === 'scissors') ||
-               (player1Choice === 'paper' && player2Choice === 'rock') ||
-               (player1Choice === 'scissors' && player2Choice === 'paper')) {
-        return 'Player1';
-    } else {
-        return 'Player2';
-    }
+
+
+function determineWinner(player1Choice, player2Choice, player3Choice) {
+  if ((player1Choice === player2Choice && player1Choice === player3Choice && player2Choice === player3Choice) ||
+    (player1Choice !== player2Choice && player1Choice !== player3Choice && player2Choice !== player3Choice) 
+    ){
+    return 'draw';
+  } else if ((player1Choice === 'rock' && player2Choice === 'paper' && player3Choice === 'paper') ||
+    (player1Choice === 'paper' && player2Choice === 'scissors' && player3Choice === 'scissors') ||
+    (player1Choice === 'scissors' && player2Choice === 'rock' && player3Choice === 'rock')|| 
+    (player1Choice === 'rock' && player2Choice === 'rock' && player3Choice === 'paper') 
+    ) {
+    return 'draw';
+  } else if ((player1Choice === 'rock' && player2Choice === 'scissors' && player3Choice === 'scissors') ||
+    (player1Choice === 'paper' && player2Choice === 'rock' && player3Choice === 'rock') ||
+    (player1Choice === 'scissors' && player2Choice === 'paper' && player3Choice === 'paper')) {
+    return 'Player 1';
+  } else if ((player2Choice === 'rock' && player1Choice === 'scissors' && player3Choice === 'scissors') ||
+    (player2Choice === 'paper' && player1Choice === 'rock' && player3Choice === 'rock') ||
+    (player2Choice === 'scissors' && player1Choice === 'paper' && player3Choice === 'paper')) {
+    return 'Player 2';
+  } else {
+    return 'Player 3';
+  }
+  
+}
+
+function resetGameData() {
+  storeData.player1.choice = "";
+  storeData.player1.ready = false;
+  storeData.player2.choice = "";
+  storeData.player2.ready = false;
+  storeData.player3.choice = "";
+  storeData.player3.ready = false;
 }
 
 const PORT = process.env.PORT || 8000;
 server.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
